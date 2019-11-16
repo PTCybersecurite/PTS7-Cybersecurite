379988e0751a864fa5b080c6878dfb79ba2a5db7
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2019-11-16 16:01:14 +0100

    Implémentation d'une partie du cours sur le Phishing

--- cours/Phishing/Contenu_phishing.php
+++ cours/Phishing/Contenu_phishing.php
@@ -29,6 +29,48 @@
 		</h1>
 	</header>
 	<br/>
+
+	<h2>Qu'est-ce que c'est ?</h2>
+	<br/>
+
+	<p>Le phishing est un terme anglais signifiant hameçonnage, et cela consiste à récupérer des données personnelles sur Internet. Le plus fréquemment, le phishing intervient via mail.</p>
+
+	<p>On reçoit généralement un message d'urgence contenant : "Votre compte va bientôt expirer", "Il y a une erreur avec votre compte", "Vous avez tenté d'effectuer une transaction", etc. Ce message à l'air de provenir d'un site de confiance comme notre banque, la CAF, les impôts, notre opérateur téléphonique, des sites de ventes en ligne, etc.</p>
+
+	<p>Le but du mail est de vous piéger en vous faisant croire qu'il y a un problème important et qu'il est urgent de régler. Il y aura souvent un lien sur lequel il faut cliquer pour résoudre le soit-disant problème et ensuite il faudra remplir un formulaire où l'on vous demandera un bon nombre de vos données personnelles, notamment vos coordonnées bancaires. Une fois les données remplies, elles sont récupérées pour être utilisées à des fins malhonnêtes.
+	</p>
+	<br/>
+
+	<h2>Comment reconnaître le phishing ?</h2>
+	<br/>
+
+	<p>C'est la partie la plus importante, personne ne veut bloquer sa carte bleue et attendre 1 mois avant de pouvoir en réutiliser une autre car on vient de cliquer sur un lien dans un mail de Zalando qui disait : "Vous avez acheter 6 paire de chaussures a 300€, nous pensons que cela soit un erreur, veuiller cliquez sur le lien ci desous pour régler le probleme"
+	</p>
+
+	<p>Comme vous avez pu le remarquer grâce à votre niveau 4 sur Orthodidacte, le message est rempli de fautes. C'est l'un des premiers signes qui dit que nous avons affaire à du phishing. N'importe quel site de ventes en ligne un peu reconnu ne ferait pas autant d'erreurs en une seule phrase.</p>
+
+	<p><strong>Voici donc les différentes méthodes pour se protéger :</strong></p>
+	<ul>
+		<li>Vérifier l'orthographe</li>
+		<p>Comme dit précedemment, une orthographe bancale doit être un signal d'alarme vous disant qu'il s'agit de phishing. (RIP les niveaux 1-2 d'Orthodidacte)<p>
+
+		<li>Se méfier de l'urgence</li>
+		<p>L'urgence est un bon procédé pour vous faire perdre vos moyens, car l'urgence nous fait agir plus vite sans forcément réfléchir à ce que l'on fait. Vous paniquerez certainement en recevant un mail de votre "banque" vous disant qu'il y a eu un retrait de 5000€ sur votre compte alors que vous n'avez rien fait, à moins que vous soyez millionaire mais si vous lisez ce cours cela m'étonnerait fortement.</p>
+
+		<li>Ne pas cliquer partout</li>
+		<p>Ne faites pas les taureaux en fonçant droit sur ce qui s'agite devant vous. Certes, quand il y a un lien, on clique généralement dessus, on veut cliquer dessur, mais il ne faut surtout pas se précipiter car cela pourrait infecter votre machine.</p>
+
+		<li>Utiliser le filtre anti-phishing du navigateur</li>
+		<p>La plupart des navigateurs Internet actuels (Google Chrome, Microsoft Edge, Mozilla Firefox, Safari) sont équipés d'une fonctionnalité qui signale les tentatives de phishing. Cela peut fonctionner différement selon les navigateurs (listes noires, listes blanches, ...) mais cet outil nous permet de rester vigilant.</p>
+
+		<li>Aller soi-même sur le site</li>
+		<p>Face à un message d'urgence de votre banque, il est nécessaire d'aller vous-même sur le site de votre banque sans passer par le lien dans le mail. Cela vous permettra de vérifier s'il y a effectivement un problème</p>
+
+		<li>Faire preuve d'un peu de bon sens</li>
+		<p>On ne le répetera jamais assez : "tout ce qui vient d'Internet n'est pas forcément vrai". Il ne faut surtout pas être crédule et ne pas croire tout ce que l'on voit.</p>
+
+	</ul>
+
 	</section>
 	
 	<footer>
