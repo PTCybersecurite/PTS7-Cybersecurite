6942eeb54f9ea09b8e451a5bc9f8aa88994be2a5
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 16:45:28 +0100

    Update Introduction.php

--- cours/Bonnes_Pratiques/Introduction.php
+++ cours/Bonnes_Pratiques/Introduction.php
@@ -32,7 +32,8 @@
     <p>Nous avons vu qu'il fallait avoir des mots de passe diversifiés et solides, et que nous devions porter une attention particulière à tous les mails "urgents" et douteux que l'on pouvait recevoir</p>
     <br/>
 
-    <p></p>
+    <p>Il faut faire attention à plusieurs choses dans la vie de tous les jours, notamment, ne pas laisser une feuille de brouillon avec un identifiant et un mot de passe écrits sur celle-ci (Je ne vise personne, surtout pas un élève de Centrale Marseille). N'importe qui pourrait essayer de se connecter à votre compte, et on pourrait tester cet identifiant et ce mot de passe sur d'autres sites. Et si vous n'avez pas suivi nos cours, vous avez certainement le même mot de passe pour plusieurs sites. Donc, on aurait accès à tous vos comptes.</p>
+    <p>Dans le même registre, il serait préférable d'éviter de brancher dans votre ordinateur. une clé usb inconnue trouvée par hasard. Cela pourrait détruire votre machine avec un (ou plusieurs) virus.</p>
 	</section>
 	
 	<footer>
