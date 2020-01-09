18501b3915a2412452a2166cef12c89dce02b9c6
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 16:53:19 +0100

    Create Evaluation.php

---
+++ cours/Bonnes_Pratiques/Evaluation.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<?php require('../../includes/header.php')?>
+
+<body>
+
+        <?php require('../../includes/Bootstrap_navbar.php')?>
+
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="../../index.php">Menu Principal <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="Contenu_bonnes_pratiques.php">Page précédente</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link disabled" href="#" aria-disabled="true">Page suivante</a>
+			  </li>
+			</ul>
+		  </div>
+		</nav>
+		
+		<hr>
+	
+	<section>
+	<header>
+		<h1>
+			<strong>Evaluation</strong>
+		</h1>
+	</header>
+	<p>Enlever le titre Evaluation, et mettre un lien vers le chapitre 2 du twine</p>
+	</section>
+
+<?php include('../../includes/footer.php')?>
