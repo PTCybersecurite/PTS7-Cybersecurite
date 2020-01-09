66018dc9a142238399f45cbb0c2a3293818e8a75
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 16:35:05 +0100

    Ajout début cours de bonnes pratiques

---
+++ cours/Bonnes_Pratiques/Introduction.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<?php require('../../includes/header.php')?>
+
+<body>
+        <?php require('../../includes/Bootstrap_navbar.php')?>
+
+			<ul class="navbar-nav ml-auto">
+			  <li class="nav-item active">
+				<a class="nav-link" href="../../index.php">Menu Principal <span class="sr-only">(current)</span></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link disabled" href="#" aria-disabled="true">Page précédente</a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="Contenu_phishing.php">Page suivante</a>
+			  </li>
+			</ul>
+		  </div>
+		</nav>
+
+	<hr>
+	
+	<section>
+	<header>
+		<h1> 
+			<strong>Introduction</strong>
+		</h1>
+	</header>
+	<br/>
+    <p>Il existe un certains nombre de bonnes pratiques à adopter dans notre monde actuel pour préserver nos données. Nous l'avons déjà vu dans les deux chapitres précédents sur les mots de passe et le phishing</p>
+    <p>Nous avons vu qu'il fallait avoir des mots de passe diversifiés et solides, et que nous devions porter une attention particulière à tous les mails "urgents" et douteux que l'on pouvait recevoir</p>
+    <br/>
+
+    <p></p>
+	</section>
+	
+	<footer>
+	</footer>
+
+<?php include('../../includes/footer.php')?>
