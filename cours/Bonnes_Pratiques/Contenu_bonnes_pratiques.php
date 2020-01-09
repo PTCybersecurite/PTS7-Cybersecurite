5c55d9ea6ec28b61bd13296e04b6aaa3d1db89bd
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 17:22:08 +0100

    Update Contenu_bonnes_pratiques.php

--- cours/Bonnes_Pratiques/Contenu_bonnes_pratiques.php
+++ cours/Bonnes_Pratiques/Contenu_bonnes_pratiques.php
@@ -34,6 +34,7 @@
 
 	<p>Le protocole HTTP (Hypertext Transfer Protocol) est un protocole de communication développé pour le web qui permet d'établir une connexion entre un navigateur (Google Chrome, Firefox, Opera, Safari, ...) et un serveur hébergeant un site internet. Ce protocole est indispensable pour pouvoir recevoir ou envoyer des informations au serveur.
 	Malheureusement, ce protocole n'est pas sécurisé, les données peuvent être interceptées telles quelles et sont lisibles aisément. Si vous voulez acheter le dernier album de Jul, et que vous rentrer vos coordonnées bancaires sur un site avec seulement un protocole HTTP, il y a de fortes chances que quelqu'un vous les vole.</p>
+	<p>C'est pourquoi le protocole HTTPS (Hypertext Transfer Protocol Secure) a été créé. Ce protocole rajoute une couche de cryptage permettant de sécuriser les données en passant par un certificat SSL (Secure Socket Layer) ou TLS (Transport Layer Security). Ainsi, les données des utilisateurs sont protégées et confidentielles, car si elles sont interceptées, elles sont illisibles. Chaque utilisateur peut vérifier si un site possède le protocole HTTPS en regardant dans son navigateur : dans la barre d'adresse, on peut voir un cadenas si le site est sécurisé.</p>
 
 	
 	</section>
