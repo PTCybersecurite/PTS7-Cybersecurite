2c80651dbcd9a58b8ae184ba4809e42bde5dbdd3
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 17:41:01 +0100

    Update Contenu_bonnes_pratiques.php

--- cours/Bonnes_Pratiques/Contenu_bonnes_pratiques.php
+++ cours/Bonnes_Pratiques/Contenu_bonnes_pratiques.php
@@ -35,6 +35,22 @@
 	<p>Le protocole HTTP (Hypertext Transfer Protocol) est un protocole de communication développé pour le web qui permet d'établir une connexion entre un navigateur (Google Chrome, Firefox, Opera, Safari, ...) et un serveur hébergeant un site internet. Ce protocole est indispensable pour pouvoir recevoir ou envoyer des informations au serveur.
 	Malheureusement, ce protocole n'est pas sécurisé, les données peuvent être interceptées telles quelles et sont lisibles aisément. Si vous voulez acheter le dernier album de Jul, et que vous rentrer vos coordonnées bancaires sur un site avec seulement un protocole HTTP, il y a de fortes chances que quelqu'un vous les vole.</p>
 	<p>C'est pourquoi le protocole HTTPS (Hypertext Transfer Protocol Secure) a été créé. Ce protocole rajoute une couche de cryptage permettant de sécuriser les données en passant par un certificat SSL (Secure Socket Layer) ou TLS (Transport Layer Security). Ainsi, les données des utilisateurs sont protégées et confidentielles, car si elles sont interceptées, elles sont illisibles. Chaque utilisateur peut vérifier si un site possède le protocole HTTPS en regardant dans son navigateur : dans la barre d'adresse, on peut voir un cadenas si le site est sécurisé.</p>
+	<br/>
+
+	<h2>PC publics et connexions publiques</h2>
+	<br/>
+
+	<p>Il faut faire attention à l'utilisation des pc publics ou des connexions publiques. Les pc publics peuvent avoir une grande utilité, notamment si on veut chercher des choses sur le web (articles de journaux, images, musiques, aller sur wikipédia, ...). Le seul problème est que ce pc ne vous appartient pas, vous ne pouvez pas savoir ce qui s'est passé dessus. Il peut y avoir des virus, suite à la mauvaise utilisation d'une des personnes, ou implentés par des personnes mal-intentionnées. Il est fortement recommandé de faire très attention si vous vous connectez à un site important (comme votre boite mail). Vos coordonnées pourraient être enregistrées par un logiciel tournant en fond. Et surtout, n'enregistrez pas vos mots de passe sur les sites, la personne d'après y aura accès.</p>
+	<p>Il en va de même pour les connexions publiques, il faut être certain de qui vous fournit votre accès internet et ce pour les mêmes raisons qui sont citées partout dans le cours.</p>
+	<br/>
+
+	<h2>Les cookies</h2>
+	<p></p>
+	<br/>
+
+	<h2>Les bloqueurs de publicités</h2>
+	<p></p>
+	<br/>
 
 	
 	</section>
