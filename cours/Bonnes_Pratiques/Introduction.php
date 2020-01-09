17c9c47e91b82579ba1e8ecb4133ef2d6bdeb267
Bloodstreader <56730555+Bloodstreader@users.noreply.github.com>
2020-01-09 17:12:57 +0100

    rajout de contenu pour bonnes pratiques

--- cours/Bonnes_Pratiques/Introduction.php
+++ cours/Bonnes_Pratiques/Introduction.php
@@ -28,12 +28,15 @@
 		</h1>
 	</header>
 	<br/>
-    <p>Il existe un certains nombre de bonnes pratiques à adopter dans notre monde actuel pour préserver nos données. Nous l'avons déjà vu dans les deux chapitres précédents sur les mots de passe et le phishing</p>
-    <p>Nous avons vu qu'il fallait avoir des mots de passe diversifiés et solides, et que nous devions porter une attention particulière à tous les mails "urgents" et douteux que l'on pouvait recevoir</p>
+    <p>Il existe un certains nombre de bonnes pratiques à adopter dans notre monde actuel pour préserver nos données. Nous l'avons déjà vu dans les deux chapitres précédents sur les mots de passe et le phishing.
+    Nous avons vu qu'il fallait avoir des mots de passe diversifiés et solides, et que nous devions porter une attention particulière à tous les mails "urgents" et douteux que l'on pouvait recevoir</p>
     <br/>
 
-    <p>Il faut faire attention à plusieurs choses dans la vie de tous les jours, notamment, ne pas laisser une feuille de brouillon avec un identifiant et un mot de passe écrits sur celle-ci (Je ne vise personne, surtout pas un élève de Centrale Marseille). N'importe qui pourrait essayer de se connecter à votre compte, et on pourrait tester cet identifiant et ce mot de passe sur d'autres sites. Et si vous n'avez pas suivi nos cours, vous avez certainement le même mot de passe pour plusieurs sites. Donc, on aurait accès à tous vos comptes.</p>
-    <p>Dans le même registre, il serait préférable d'éviter de brancher dans votre ordinateur. une clé usb inconnue trouvée par hasard. Cela pourrait détruire votre machine avec un (ou plusieurs) virus.</p>
+    <p>Il faut faire attention à plusieurs choses dans la vie de tous les jours, notamment, ne pas laisser une feuille de brouillon avec un identifiant et un mot de passe écrits sur celle-ci (Je ne vise personne, surtout pas un élève de Centrale Marseille). N'importe qui pourrait essayer de se connecter à votre compte, et on pourrait tester cet identifiant et ce mot de passe sur d'autres sites. Et si vous n'avez pas suivi nos cours, vous avez certainement le même mot de passe pour plusieurs sites. Donc, on aurait accès à tous vos comptes.
+    Dans le même registre, il serait préférable d'éviter de brancher dans votre ordinateur. une clé usb inconnue trouvée par hasard. Cela pourrait détruire votre machine avec un (ou plusieurs) virus.</p>
+    <br/>
+
+    <p>Mais ce n'est pas le point principal de ce chapitre, nous allons parler des bonnes habitudes à avoir pour naviguer en sécurité sur le web.</p>
 	</section>
 	
 	<footer>
